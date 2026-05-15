6ffaa2260cd0ffdafe1d279f36b251af061fe569
Maycol Muñoz Valencia <maycolmunozv@gmail.com>
2026-05-14 23:04:51 -0500

    ya cree 4 de las 5 funciones(falta el de eliminar placa), en el 1 falta agregar un control de error por si no ingresa una placa, tambian un comando en todas para que las plcas ingresadas se pongan en mayuscula y todas se guarden igual

--- funciones.py
+++ funciones.py
@@ -1,19 +1,41 @@
-
-
 def agregar_placa():
-    print ("crear")
+    import datetime
+
+    placa = input("Ingresa la placa del vehiculo: ")
+    archivo = open("data/vehiculos.txt", "a")
+    archivo.write(f"{placa} \n")
+    archivo.close()
+
+    fecha_actual = datetime.datetime.now()
+    archivo_historial = open("data/historial.txt", "a")
+    archivo_historial.write(f"La placa {placa} ingreso: {fecha_actual}\n")
+    archivo_historial.close()
+
+    print(f"Placa {placa} agregada correctamente")
 
 def mostrar_placas():
-    print ("mostrar")
+    archivo = open("data/vehiculos.txt","r")
+    contenido = archivo.read()
+    print("Las placas actualmente en el parqueadero son:\n",contenido)
+    archivo.close()
 
 def buscar_placa():
-    print ("buscar")
+    archivo = open("data/vehiculos.txt", "r")
+    busqueda = archivo.read()
+    buscar = input("Ingresa la placa que quieres buscar: ")
+    
+    if buscar in busqueda:
+        print(f"El vehiculo con placa: {buscar}, si se encuentra en el parqueadero.")
+
+    else:
+        print(f"El vehiculo con placa: {buscar}, no se encuentra en el parqueadero.")
+
+    archivo.close()
 
 def eliminar_placa():
     print ("eliminar")
 
 def historial_placas():
-    print ("historial")
-
-
-
+    archivo = open("data/historial.txt", "r")
+    busqueda = archivo.read()
+    print(busqueda)
